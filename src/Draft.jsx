ab0fca69d117d5eed27ecf6819de37e0345d97ed
Philip Kuperberg <philipkuperberg@gmail.com>
2018-10-17 15:21:38 -0400

    converted Draft into a context component so we can tap into Provider in other child components; improved value comparison

--- src/Draft.jsx
+++ src/Draft.jsx
@@ -1,17 +1,54 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import update from 'immutability-helper';
-import { isEqual } from 'lodash';
+import isEqualWith from 'lodash/isEqualWith';
 import PropTypes from 'prop-types';
 
 
-export default
-   class Draft extends PureComponent {
+export function stringify(it){
+   // objects and arrays = JSON
+   if (typeof it === 'object'){
+      return JSON.stringify(it);
+   }
+   // undefined = ''
+   if (typeof it === 'undefined'){
+      return '';
+   }
+   // NaN, null, etc...
+   return `${it}`;
+}
 
-   static propTypes = {
-      children: PropTypes.func
+export function compareValues(a, b, strict=false){
+   return strict
+      ? a === b
+      : stringify(a) === stringify(b);
+}
+
+export let DraftContext = React.createContext('draft-context')
+export let DraftConsumer = DraftContext.Consumer;
+
+export default class Draft extends PureComponent{
+   render(){
+      let {
+         original,
+         children
+      } = this.props;
+      return typeof children === 'function'
+         ? (
+            <DraftProvider original={original}>
+               <DraftConsumer>{
+                  props => children(props)
+               }</DraftConsumer>
+            </DraftProvider>
+         ) : children;
+   }
+}
+
+export class DraftProvider extends PureComponent{
+   static defaultProps = {
+      stringifyCompare: true
    }
 
-   constructor(props) {
+   constructor(props){
       super(props);
       this.state = {};
    }
@@ -19,7 +56,7 @@ export default
    /**
     * gets a value by key
     */
-   get = key =>
+   get = key => 
       this.state[key] === undefined
          ? this.props.original[key]
          : this.state[key]
@@ -30,8 +67,8 @@ export default
    set = (key, value) =>
       this.setState(
          typeof key === 'object'
-            ? key
-            : { [key]: value }
+         ? key
+         : {[key]: value}
       )
 
    /**
@@ -42,39 +79,104 @@ export default
       this.setState(
          update(this.state, updateObj)
       )
+   
+   stringify = it => {
+      // objects and arrays
+      if (typeof it === 'object'){
+         return JSON.stringify(it);
+      }
+      if (typeof it === 'undefined'){
+         return '';
+      }
+      // NaN, null, etc...
+      return `${it}`;
+   }
+   
+   compare = (a, b) => {
+      let {stringifyCompare} = this.props;
+      return stringifyCompare
+         ? this.stringify(a) === this.stringify(b)
+         : a == b;
+   }
+   
+   /**
+    * returns an object with state of data
+    * and an array of object keys that have been edited
+    */
+   getState = () => {
+      let {original} = this.props;
+      let edits = {...original};
+      let keysChanged = {};
+      for (let k in this.state){
+         if (this.state[k] !== undefined){
+            if (!this.compare(original[k], this.state[k])){
+               keysChanged[k] = {
+                  original: original[k],
+                  edited: this.state[k]
+               };
+            }
+            edits[k] = this.state[k];
+         }
+      }
+      return {
+         changed: keysChanged,
+         state: edits
+      };  
+   }
 
    /**
-    * returns an object representing
-    * current state, with edits
+    * clear changes from state
     */
-   getState = () => ({
-      ...this.props.original,
-      ...this.state
-   })
+   clear = () => {
+      let update = {};
+      for (let k in this.state){
+         update[k] = undefined;
+      }
+      this.setState(update);
+   }
 
    /**
     * Returns true to indicate
     * changes have been made
     */
    check = () =>
-      isEqual(
+      isEqualWith(
          this.props.original,
-         this.getState()
+         this.getState().state,
+         this.compare
       ) ? false : true
 
-   render() {
+   render(){
       let {
-         children
-      } = this.props;
+         props: {
+            children
+         },
+         get,
+         set,
+         update,
+         clear,
+         check,
+         getState,
+      } = this;
+      let {
+         changed,
+         state
+      } = getState();
 
-      return typeof children === 'function'
-         ? children({
-            get: this.get,
-            set: this.set,
-            update: this.update,
-            check: this.check,
-            state: this.getState()
-         })
-         : children;
+      return (
+         <DraftContext.Provider
+            value={{
+               get,
+               set,
+               update,
+               clear,
+               check,
+               state,
+               changed
+            }}
+            children={children}
+         />
+      )
    }
 }
+
